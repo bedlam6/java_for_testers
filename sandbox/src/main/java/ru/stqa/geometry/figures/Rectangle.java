32d309e1932c2d001562a2a3d4c3f7488e65b680
bedlam6 <russthug@gmail.com>
2026-07-30 21:57:08 +0700

    Созданы пакеты

---
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -0,0 +1,11 @@
+package ru.stqa.geometry.figures;
+
+public class Rectangle {
+   public static void printRectangleArea(double a, double b) {
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + RectangleArea(a, b));
+    }
+
+    private static double RectangleArea(double a, double b) {
+        return a * b;
+    }
+}
