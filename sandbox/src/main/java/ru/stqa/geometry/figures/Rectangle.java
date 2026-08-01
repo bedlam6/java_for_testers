a019112386483375d4ad25197fe4a4d7bd857bf5
bedlam6 <russthug@gmail.com>
2026-08-01 19:38:58 +0700

    Рассчитан периметр квадрата

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -2,11 +2,11 @@ package ru.stqa.geometry.figures;
 
 public class Rectangle {
    public static void printRectangleArea(double a, double b) {
-       var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a, b, RectangleArea(a, b))
+       var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a, b, RectangleArea(a, b));
         System.out.println(text);
     }
 
-    private static double RectangleArea(double a, double b) {
+    public static double RectangleArea(double a, double b) {
         return a * b;
     }
 }
---
+++ sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.geometry.figures;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SquareTests  {
+
+    @Test
+    void canCalculateArea() {
+        double result = Square.area(5.0);
+        Assertions.assertEquals(25.0, result);
+
+    }
+
+    @Test
+    void canCanculatePerimeter() {
+        Assertions.assertEquals(20, Square.perimeter(5.0));
+    }
+}
